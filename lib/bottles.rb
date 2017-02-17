e66987bfb6c7d35c12eb67fce59580cb8e725d5c
Jack Noble <jack.noble@gmail.com>
2017-02-16 22:15:52 -0800

    Something more Sandi Metzy

--- lib/bottles.rb
+++ lib/bottles.rb
@@ -1,9 +1,82 @@
+module BottlesRemaining
+  def self.from_verse(n)
+    case n
+    when 0
+      NoMore.new
+    when 1
+      OneMore.new
+    else
+      ManyMore.new(n)
+    end
+  end
+
+  class NoMore
+    def to_s
+      "no more bottles"
+    end
+
+    def drink_or_shop
+      "Go to the store and buy some more"
+    end
+
+    def next_number
+      BottlesRemaining.from_verse(99)
+    end
+
+    def capitalize
+      to_s.capitalize
+    end
+  end
+
+  class OneMore
+    def to_s
+      "1 bottle"
+    end
+
+    def drink_or_shop
+      "Take it down and pass it around"
+    end
+
+    def next_number
+      BottlesRemaining.from_verse(0)
+    end
+
+    def capitalize
+      to_s.capitalize
+    end
+  end
+
+  class ManyMore
+    def initialize(n)
+      @num = n
+    end
+
+    def to_s
+      "#{@num} bottles"
+    end
+
+    def drink_or_shop
+      "Take one down and pass it around"
+    end
+
+    def next_number
+      BottlesRemaining.from_verse(@num - 1)
+    end
+
+    def capitalize
+      to_s.capitalize
+    end
+  end
+end
+
 class Bottles
   def verse(n)
+    bottles = BottlesRemaining.from_verse(n)
+
     <<-STR
-#{bottles(n).capitalize} of beer on the wall, #{bottles(n)} of beer.
-#{strategy_for_drinking_more(n)}, #{bottles((n - 1) % 100)} of beer on the wall.
-     STR
+#{bottles.capitalize} of beer on the wall, #{bottles} of beer.
+#{bottles.drink_or_shop}, #{bottles.next_number} of beer on the wall.
+    STR
   end
 
   def verses(start, fin)
@@ -12,29 +85,5 @@ class Bottles
 
   def song
     verses(99, 0)
-  end
-
-  private
-
-  def bottles(n)
-    case n
-    when 0
-      "no more bottles"
-    when 1
-      "1 bottle"
-    else
-      "#{n} bottles"
-    end
-  end
-
-  def strategy_for_drinking_more(n)
-    case n
-    when 0
-      "Go to the store and buy some more"
-    when 1
-      "Take it down and pass it around"
-    else
-      "Take one down and pass it around"
-    end
   end
 end
